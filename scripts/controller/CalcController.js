6a6d066c3a3e584dd3c65c162f30ba2fe9bc9fc9
Tiago <tiagoveronesi@gmail.com>
2018-09-11 02:11:12 -0300

    Add EventListener vários eventos ao mesmo elemento
    - Desenvolvimento de metódo para addEventListenerAll que recebe por parâmetro qual elemento irá receber os novos eventos e a qual escopo irá aplicar.
    - Adição de evento para que quando o mouse estiver sobre o botão ou texto altere o cursor do mouse para pointer.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -24,15 +24,25 @@ class CalcController {
         this._displayCalcEl.innerHTML = "0";
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event=>{
+            // enviar o parâmetro false, para que quando o clique for realizado 
+            // no texto ou na img do botão não ocorra duas vezes o mesmo evento
+            element.addEventListener(event, fn, false);
+        });
+    }
+
     initButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach(btn=>{
 
-            btn.addEventListener('click', e=>{
-                
-                console.log(btn.className.baseVal.replace("btn-",""));
+            this.addEventListenerAll(btn, "click drag", e=>{
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
 
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
+                btn.style.cursor = "pointer";
             });
 
         })
