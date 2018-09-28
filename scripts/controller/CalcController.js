edeaf05289e7c3c279f874d4e6ff722fae209318
Tiago <tiagoveronesi@gmail.com>
2018-09-27 23:07:03 -0300

    Adição de controles
    - Verificar qual botão foi clicado e executar ação;
    - Tratativa para quando clicar em números consecutivamente, concatenar os valores para compor  o número que será utilizado na operação;
    - Tratativa para quando clicar sobre os botões de operações consecutivamente, para substituir, prevalecerá o último valor;

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,7 @@
 class CalcController {
 
     constructor(){
-
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -26,19 +26,123 @@ class CalcController {
 
     addEventListenerAll(element, events, fn){
         events.split(' ').forEach(event=>{
-            // enviar o parâmetro false, para que quando o clique for realizado 
-            // no texto ou na img do botão não ocorra duas vezes o mesmo evento
+            /**
+             *  Enviar o parâmetro false, para que quando o clique for realizado
+             * no texto ou na img do botão não ocorra duas vezes o mesmo evento
+            */
             element.addEventListener(event, fn, false);
         });
     }
 
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    getLastOperation(){
+        return this._operation[this._operation.length-1];
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value){
+
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1);
+
+    }
+
+
+    addOperation(value){
+        if (isNaN(this.getLastOperation())) {
+            // Verificar se é um operador
+            if (this.isOperator(value)) {
+
+                this.setLastOperation(value);
+
+            } else if(isNaN(value)) {
+                console.log(value);
+            } else {
+                this._operation.push(value);
+            }
+
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+    
+    execBtn(value){
+
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                this.addOperation('+');
+                break;
+            case 'subtracao':
+                this.addOperation('-');
+                break;
+            case 'divisao':
+                this.addOperation('/');
+                break;
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+            case 'porcento':
+                this.addOperation('%');
+                break;
+            case 'igual':
+                
+                break;
+            case 'ponto':
+                this.addOperation('.');
+                break;
+            
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+        }
+    }
+
     initButtonsEvents(){
+
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach(btn=>{
 
             this.addEventListenerAll(btn, "click drag", e=>{
-                console.log(btn.className.baseVal.replace("btn-", ""));
+
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
+
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
